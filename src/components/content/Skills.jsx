30db1f02b1c08246d913d7299179e4ec84873d0e
Abel <abellx3@gmail.com>
2023-03-24 10:21:40 -0700

    refactor: add blurbs for each section

--- src/components/content/Skills.jsx
+++ src/components/content/Skills.jsx
@@ -11,15 +11,17 @@ export default function Skills() {
                         Skills &amp; Technologies
                     </h1>
                     <p className="mx-auto text-base leading-relaxed lg:w-3/4 xl:w-2/4">
-                        Lorem ipsum dolor sit amet consectetur, adipisicing
-                        elit. Nisi sit ipsa delectus eum quo voluptas aspernatur
-                        accusantium distinctio possimus est.
+                        Get an overview of my technical skills, which have been
+                        honed through years of experience and continuous
+                        learning. This comprehensive list showcases my expertise
+                        and qualifications, reflecting my commitment to being a
+                        proficient and skilled professional in my field.
                     </p>
                 </div>
                 <div className="-mx-2 flex flex-wrap sm:mx-auto sm:mb-2 lg:w-4/5">
                     {Object.entries(skills).map(([skill, icon]) => (
                         <div key={skill} className="w-full p-2 xs:w-1/2">
-                            <div className="flex h-full rounded bg-gray-800 p-4 gap-4">
+                            <div className="flex h-full gap-4 rounded bg-gray-800 p-4">
                                 {icon}
                                 <span className="font-medium text-white">
                                     {skill}
