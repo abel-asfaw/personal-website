32e742a7e0a23eb4b06875394300f4155afef3d7
Abel <abellx3@gmail.com>
2023-03-29 14:46:10 -0700

    fix

--- src/components/common/Button.jsx
+++ src/components/common/Button.jsx
@@ -10,9 +10,9 @@ export default function Button({
 }) {
     const classes = classNames(className, {
         'duration-200 ease-in hover:text-white': navButton || promptButton,
-        'rounded-full px-3 py-1.5 text-sm': pillButton,
-        'rounded border-0 py-2 px-6 text-lg focus:outline-none': promptButton,
         'p-1 text-lg': navButton,
+        'rounded border-0 py-2 px-6 text-lg focus:outline-none': promptButton,
+        'rounded-full px-3 py-1.5 text-sm': pillButton,
     });
 
     return (
