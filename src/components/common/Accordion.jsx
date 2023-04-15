27074d1747d61d9271b22f797fb8832abe19e8d7
Abel <dev@abelasfaw.com>
2023-04-15 16:17:37 -0700

    style: add animation for accordion component

--- src/components/common/Accordion.jsx
+++ src/components/common/Accordion.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
-import { HiOutlinePlusSm, HiOutlineMinusSm } from 'react-icons/hi';
-import Button from './Button';
-import classNames from 'classnames';
+import { AnimatePresence, motion } from 'framer-motion';
+import { HiOutlineMinusSm, HiOutlinePlusSm } from 'react-icons/hi';
+import Content from './AccordionContent';
 
 export default function Accordion({ items }) {
     const [expandedIndex, setExpandedIndex] = useState(-1);
@@ -12,50 +12,73 @@ export default function Accordion({ items }) {
         );
     }
 
+    function renderIcon(isExpanded) {
+        return (
+            <motion.div
+                initial={{ rotate: isExpanded ? 90 : 0 }}
+                animate={{ rotate: isExpanded ? 0 : 90 }}
+                transition={{ duration: 0.2 }}
+            >
+                {isExpanded ? (
+                    <HiOutlineMinusSm size={24} />
+                ) : (
+                    <HiOutlinePlusSm size={24} />
+                )}
+            </motion.div>
+        );
+    }
+
     const renderedItems = items.map((item, index) => {
         const isExpanded = index === expandedIndex;
-        const classes = classNames(
-            'mb-4 flex h-full cursor-pointer items-center gap-3 rounded p-4 duration-200 sm:hover:bg-gray-700',
-            { 'bg-gray-800': !isExpanded, 'bg-gray-700': isExpanded }
-        );
-        const icon = isExpanded ? (
-            <HiOutlineMinusSm size={24} />
-        ) : (
-            <HiOutlinePlusSm size={24} />
-        );
+        const { title, date, description, skills } = item;
 
         return (
-            <div key={item.title}>
-                <div className={classes} onClick={() => handleClick(index)}>
-                    <div className="text-green-400">{icon}</div>
-                    <div className="flex flex-1 flex-col gap-1 sm:flex-row sm:justify-between">
-                        <span className="font-medium text-white">
-                            {item.title}
-                        </span>
-                        <span className="sm:text-right">{item.date}</span>
+            <div key={title}>
+                <motion.div
+                    animate={{
+                        backgroundColor: isExpanded ? '#374151' : '#1F2937',
+                    }}
+                    className="mt-4 flex h-full cursor-pointer items-center gap-3 rounded p-4"
+                    onClick={() => handleClick(index)}
+                >
+                    <AnimatePresence initial={false}>
+                        <div className="text-green-400">
+                            {renderIcon(isExpanded)}
+                        </div>
+                    </AnimatePresence>
+                    <div className="flex flex-1 flex-col sm:flex-row sm:justify-between">
+                        <span className="font-medium text-white">{title}</span>
+                        <span className="sm:text-right">{date}</span>
                     </div>
-                </div>
-                {isExpanded && (
-                    <div className="mb-4 flex h-full flex-wrap rounded border-2 border-gray-800 bg-gray-900 p-4 text-white">
-                        <span>{item.description}</span>
-                        {!!item.skills.length && (
-                            <span className="mt-2 flex flex-wrap gap-2">
-                                {item.skills.map((skill, index) => (
-                                    <Button
-                                        pillButton
-                                        className="bg-indigo-600"
-                                        key={index}
-                                    >
-                                        {skill}
-                                    </Button>
-                                ))}
-                            </span>
-                        )}
-                    </div>
-                )}
+                </motion.div>
+                <AnimatePresence initial={false}>
+                    {isExpanded && (
+                        <motion.div
+                            key="content"
+                            initial="collapsed"
+                            animate="open"
+                            exit="collapsed"
+                            variants={{
+                                open: { opacity: 1, height: 'auto' },
+                                collapsed: { opacity: 0, height: 0 },
+                            }}
+                            transition={{
+                                duration: 0.4,
+                                ease: [0, 0, 0, 1],
+                            }}
+                        >
+                            <motion.div className="pt-4">
+                                <Content
+                                    description={description}
+                                    skills={skills}
+                                />
+                            </motion.div>
+                        </motion.div>
+                    )}
+                </AnimatePresence>
             </div>
         );
     });
 
-    return <div className="w-full">{renderedItems}</div>;
+    return <div className="-mt-4 w-full">{renderedItems}</div>;
 }
