baa9b11914e32a414c13a0714ab3bdb36c946bbf
Abel <abellx3@gmail.com>
2023-04-15 16:29:49 -0700

    refactor: clean up section margins

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -13,16 +13,14 @@ export default function Skills() {
 
     return (
         <Section id={id} title={title} blurb={blurb}>
-            <div className="-mx-2 flex flex-wrap sm:mx-auto sm:mb-2 lg:w-4/5">
+            <div className="-mx-2 -mt-2 flex flex-wrap sm:mx-auto lg:w-4/5">
                 {Object.entries(skills).map(([skill, icon]) => (
                     <div key={skill} className="w-full p-2 xs:w-1/2">
                         <div className="flex h-full items-center gap-4 rounded bg-gray-800 p-4">
                             <span className="[&>*]:h-6 [&>*]:w-6 [&>*]:text-green-400">
                                 {icon}
                             </span>
-                            <span className="text-white">
-                                {skill}
-                            </span>
+                            <span className="text-white">{skill}</span>
                         </div>
                     </div>
                 ))}
