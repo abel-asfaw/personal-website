b611cb18afe30c502bbe86e40834861aa9895aab
Abel <dev@abelasfaw.com>
2023-04-19 12:48:58 -0700

    refactor: create PillButton component and move logic from Button component into it

--- src/components/common/AccordionContent.jsx
+++ src/components/common/AccordionContent.jsx
@@ -1,9 +1,10 @@
 import { motion } from 'framer-motion';
-import Button from './Button';
+import PillButton from './PillButton';
 
 export default function Content({ description, skills }) {
     return (
         <motion.div
+            aria-label="Accordion content"
             variants={{ collapsed: { scale: 0.8 }, expanded: { scale: 1 } }}
             transition={{ duration: 0.4 }}
             className="flex h-full flex-wrap rounded border-2 border-gray-800 bg-gray-900 p-4 text-white"
@@ -12,13 +13,9 @@ export default function Content({ description, skills }) {
             {!!skills.length && (
                 <span className="flex flex-wrap gap-2 pt-2">
                     {skills.map(skill => (
-                        <Button
-                            key={skill}
-                            pillButton
-                            className="bg-indigo-600 font-medium"
-                        >
+                        <PillButton key={skill} className="bg-indigo-600">
                             {skill}
-                        </Button>
+                        </PillButton>
                     ))}
                 </span>
             )}
