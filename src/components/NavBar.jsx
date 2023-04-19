5a96908dc343d962cc294a02ff4b5060dcb8cf1b
Abel <dev@abelasfaw.com>
2023-04-18 17:37:33 -0700

    refactor: change NavBar background color and 'Abel Asfaw' btn -> 'About'

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,12 +2,12 @@ import Button from './common/Button';
 
 export default function Navbar() {
     return (
-        <header className="top-0 z-10 bg-gray-800 p-3 drop-shadow-xl sm:sticky sm:p-5">
-            <div className="container mx-auto flex flex-col items-center justify-between font-cubano sm:flex-row">
-                <nav className="p-1 text-2xl text-white">
-                    <a href="#about">Abel Asfaw</a>
-                </nav>
-                <nav className="flex gap-2 sm:mr-auto sm:ml-4 sm:border-l sm:border-gray-700 sm:p-0 sm:pl-4">
+        <header className="sticky top-0 z-10 bg-gray-925 p-2">
+            <div className="container mx-auto flex flex-col font-cubano">
+                <nav className="flex flex-wrap justify-center gap-2 sm:gap-10">
+                    <Button navButton href="#about">
+                        About
+                    </Button>
                     <Button navButton href="#experience">
                         Experience
                     </Button>
