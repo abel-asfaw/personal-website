51459f251646d31fccb4ab2df9e6911888c73a78
Abel <abellx3@gmail.com>
2023-04-19 00:34:30 -0700

    style: add partial transparency and background blur to NavBar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,7 +2,7 @@ import Button from './common/Button';
 
 export default function Navbar() {
     return (
-        <header className="sticky top-0 z-10 bg-gray-925 p-2">
+        <header className="sticky top-0 z-10 bg-gray-925/80 backdrop-blur drop-shadow-sm p-4">
             <div className="container mx-auto flex flex-col font-cubano">
                 <nav className="flex flex-wrap justify-center gap-2 sm:gap-10">
                     <Button navButton href="#about">
