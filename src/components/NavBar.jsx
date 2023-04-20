1923f6a2fe56d16578047723b4a72a567e58a708
Abel <abellx3@gmail.com>
2023-04-20 10:17:46 -0700

    style: add NavBar drop shadow

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,7 +2,7 @@ import LinkButton from './common/LinkButton';
 
 export default function Navbar() {
     return (
-        <header className="sticky top-0 z-10 bg-gray-925/80 backdrop-blur drop-shadow-sm p-4">
+        <header className="sticky top-0 z-10 bg-gray-925/70 p-4 shadow-sm shadow-gray-925 backdrop-blur">
             <div className="container mx-auto flex flex-col font-cubano">
                 <nav className="flex flex-wrap justify-center gap-2 sm:gap-10">
                     <LinkButton navButton href="#about">
