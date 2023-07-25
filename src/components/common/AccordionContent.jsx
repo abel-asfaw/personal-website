73f91437023f13adc26e8a65f217109aba9b12b0
Abel <dev@abelasfaw.com>
2023-07-24 20:12:01 -0700

    style: change text style/color for accordion description

--- src/components/common/AccordionContent.jsx
+++ src/components/common/AccordionContent.jsx
@@ -9,7 +9,7 @@ export default function Content({ description, skills }) {
             transition={{ duration: 0.4 }}
             className="flex h-full flex-wrap rounded border-2 border-gray-800 bg-gray-900 p-4 text-white"
         >
-            <span>{description}</span>
+            <span className="leading-relaxed text-gray-200">{description}</span>
             {!!skills.length && (
                 <span className="flex flex-wrap gap-2 pt-2">
                     {skills.map(skill => (
