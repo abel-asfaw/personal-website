03a64385d714f803644e81c38293e0b654c9526d
Abel <dev@abelasfaw.com>
2023-08-21 18:53:39 -0700

    refactor: make text editing easier by using paragraph elements

--- src/data/projects.jsx
+++ src/data/projects.jsx
@@ -2,43 +2,56 @@ export const projects = [
     {
         title: 'Personal Website',
         subtitle: 'JavaScript, React, Tailwind CSS, Vite.js',
-        description:
-            "Fully responsive web application showcasing my experience, skills, and projects in order to\
-                provide an online presence for potential employers. You're\
-                already looking at the demo, so click to see source code 🥳",
+        description: (
+            <p>
+                Fully responsive web application showcasing my experience,
+                skills, and projects in order to provide an online presence for
+                potential employers. You're already looking at the demo, so
+                click to see source code.
+            </p>
+        ),
         image: '/assets/svg/a-background.svg',
         link: 'https://github.com/abel-asfaw/personal-website',
     },
     {
         title: 'Coming soon...',
         subtitle: 'Node, React, Docker, Kubernetes',
-        description:
-            'Lorem ipsum dolor sit amet consectetur adipisicing elit.\
-                Praesentium aspernatur a aut earum dolore non eligendi quasi\
-                labore laudantium quae nobis tenetur quam iure nihil tempora\
-                reprehenderit, maxime ipsum.',
+        description: (
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Praesentium aspernatur a aut earum dolore non eligendi quasi
+                labore laudantium quae nobis tenetur quam iure nihil tempora
+                reprehenderit, maxime ipsum.
+            </p>
+        ),
         image: '/assets/images/project-2.webp',
         link: 'https://github.com/abel-asfaw/',
     },
     {
         title: 'Coming soon...',
         subtitle: 'Python, FastAPI, Postgres, Docker',
-        description:
-            'Lorem ipsum dolor sit amet consectetur adipisicing elit.\
-                Praesentium aspernatur a aut earum dolore non eligendi quasi\
-                labore laudantium quae nobis tenetur quam iure nihil tempora\
-                reprehenderit, maxime ipsum.',
+        description: (
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Praesentium aspernatur a aut earum dolore non eligendi quasi
+                labore laudantium quae nobis tenetur quam iure nihil tempora
+                reprehenderit, maxime ipsum.
+            </p>
+        ),
         image: '/assets/images/project-3.webp',
         link: 'https://github.com/abel-asfaw/',
     },
     {
         title: 'Coming soon...',
         subtitle: 'Python, React, FastAPI, S3, Postgres, Docker',
-        description:
-            'Lorem ipsum dolor sit amet consectetur adipisicing elit.\
-                Praesentium aspernatur a aut earum dolore non eligendi quasi\
-                labore laudantium quae nobis tenetur quam iure nihil tempora\
-                reprehenderit, maxime ipsum.',
+        description: (
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Praesentium aspernatur a aut earum dolore non eligendi quasi
+                labore laudantium quae nobis tenetur quam iure nihil tempora
+                reprehenderit, maxime ipsum.
+            </p>
+        ),
         image: '/assets/images/project-4.webp',
         link: 'https://github.com/abel-asfaw/',
     },
