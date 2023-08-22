86a223035c7a633ac774d3e20b035304a8e47e95
Abel <dev@abelasfaw.com>
2023-08-22 00:20:16 -0700

    edit: refresh the job descriptions of each experience

--- src/data/experience.jsx
+++ src/data/experience.jsx
@@ -4,9 +4,10 @@ export const experience = [
         date: 'Apr. 2023 - Present',
         description: (
             <p>
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nisi
-                sit ipsa delectus eum quo voluptas aspernatur accusantium
-                distinctio possimus est.
+                At Lockheed Martin, I play a pivotal role in a specialized team.
+                Our focus is on driving the modernization and enhancement of
+                maintenance systems software to meet the evolving demands of
+                aerospace defense.
             </p>
         ),
         skills: [],
@@ -16,16 +17,11 @@ export const experience = [
         date: 'Jan. 2022 - Nov. 2022',
         description: (
             <p>
-                As a Software Developer on the Selling and Listing Experience
-                (SALE) and Concierge engineering teams, I played a key role in
-                enhancing the agent-facing search filters, which led to a 30%
-                increase in positive user feedback. In addition to developing
-                email templates for the Concierge notification system, I
-                actively collaborated with cross-functional teams to ensure
-                seamless project execution. I also documented Conductor
-                workflows, significantly increasing team productivity. By
-                interviewing team members, I identified and addressed core
-                issues, improving overall engineering processes.
+                I initially joined the Selling and Listing Experience (SALE)
+                team at Redfin, focusing on full-stack development. Later, I
+                transitioned to the Concierge team, where our primary focus was
+                to develop tools and processes to assist agents in streamlining
+                home renovation processes.
             </p>
         ),
         skills: ['Java', 'JavaScript', 'React', 'LESS', 'Postgres', 'GraphQL'],
@@ -35,13 +31,10 @@ export const experience = [
         date: 'Aug. 2020 - Jan. 2022',
         description: (
             <p>
-                As a Teaching Assistant in UMBC's Computer Science Department, I
-                guided 20 students through weekly exercises in Python, held
-                office hours for personalized assistance, and contributed to
-                assignment creation. I received positive feedback from 90% of
-                students and consistently improved my teaching methods based on
-                their input. My expertise in teaching complex coding concepts
-                was crucial in helping students of varying skill levels succeed.
+                As a beacon of support in the UMBC Department of Computer
+                Science, I mentored 20 students each week, ensured they
+                understood fundamental coding concepts, held consistent office
+                hours, and even brainstormed new assignment ideas.
             </p>
         ),
         skills: ['Python'],
@@ -51,14 +44,10 @@ export const experience = [
         date: 'Mar. 2021 - Dec. 2021',
         description: (
             <p>
-                As a Computer Science Tutor, I led tutoring sessions for two
-                introductory computer science courses focusing on Python
-                programming fundamentals and OOP principles in C++. I worked
-                with 1 to 4 students per session, fostering strong relationships
-                that created a positive and supportive learning environment. My
-                dedication, effectiveness, and supportive approach earned praise
-                from faculty members, who acknowledged my commitment to helping
-                students understand complex programming concepts and techniques.
+                At the UMBC Academic Success Center, I led tutoring sessions for
+                two fundamental computer science courses, honing in on Python
+                basics and the OOP principles of C++, ensuring students grasped
+                key concepts with ease.
             </p>
         ),
         skills: ['Python', 'C++'],
@@ -68,15 +57,11 @@ export const experience = [
         date: 'May 2021 - Aug. 2021',
         description: (
             <p>
-                During my internship at Northrop Grumman, I automated build and
-                deployment processes for large-scale ground system solutions in
-                an agile environment. By using Ansible, I reduced the Minio
-                deployment process time from 2 hours to just 6 minutes and saved
-                9 minutes daily on the privilege escalation process for testers
-                and developers. I also coached fellow interns on Ansible
-                adoption, further promoting its use within the organization.
+                My internship at Northrop Grumman saw me pioneering the
+                automation of build and deployment processes for large-scale
+                ground system solutions in an agile environment.
             </p>
         ),
-        skills: ['Ansible', 'Docker'],
+        skills: ['Ansible'],
     },
 ];
