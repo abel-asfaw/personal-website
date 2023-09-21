6d373dc6950abeef4f787d3403bd574ec6db22d0
Abel <dev@abelasfaw.com>
2023-09-21 13:26:18 -0700

    refactor: merge div with nav element as it is an unecessary parents

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -10,20 +10,18 @@ export default function Navbar() {
 
     return (
         <header className="sticky top-0 z-10 bg-zinc-950/70 p-4 shadow-sm shadow-zinc-950 backdrop-blur">
-            <div className="container mx-auto flex flex-col font-cubano">
-                <nav className="flex flex-wrap justify-center gap-2 sm:gap-10">
-                    {navItems.map((item) => (
-                        <LinkButton
-                            className="text-lg text-neutral-400 hover:text-white"
-                            key={item.label}
-                            href={item.href}
-                            navButton
-                        >
-                            {item.label}
-                        </LinkButton>
-                    ))}
-                </nav>
-            </div>
+            <nav className="flex flex-wrap justify-center gap-2 font-cubano sm:gap-16">
+                {navItems.map((item) => (
+                    <LinkButton
+                        key={item.label}
+                        href={item.href}
+                        className="text-lg text-neutral-400 hover:text-white"
+                        navButton
+                    >
+                        {item.label}
+                    </LinkButton>
+                ))}
+            </nav>
         </header>
     );
 }
