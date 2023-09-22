109133bb6f470e11e2f896482e5ab69b956efbad
Abel <dev@abelasfaw.com>
2023-09-21 23:52:16 -0700

    clean: change data files from .jsx to .js

--- src/data/experience.js
+++ src/data/experience.js
@@ -2,66 +2,56 @@ const experience = [
     {
         title: 'Software Engineer @ Lockheed Martin',
         date: 'Apr. 2023 – Present',
-        description: (
-            <p>
+        description: `
                 At Lockheed Martin, I play a pivotal role in a specialized team.
                 Our focus is on driving the modernization and enhancement of
                 maintenance systems software to meet the evolving demands of
                 aerospace defense.
-            </p>
-        ),
+            `,
         skills: ['Java', 'JavaScript', 'Docker', 'Kubernetes', 'Helm'],
     },
     {
         title: 'Software Developer @ Redfin',
         date: 'Jan. 2022 – Nov. 2022',
-        description: (
-            <p>
+        description: `
                 I initially joined the Selling and Listing Experience (SALE)
                 team at Redfin, focusing on full-stack development. Later, I
                 transitioned to the Concierge team, where our primary focus was
                 to develop tools and processes to assist agents in streamlining
                 home renovation processes.
-            </p>
-        ),
+            `,
         skills: ['Java', 'JavaScript', 'React', 'LESS', 'Postgres', 'GraphQL'],
     },
     {
         title: 'Teaching Assistant @ UMBC',
         date: 'Aug. 2020 – Jan. 2022',
-        description: (
-            <p>
+        description: `
                 As a beacon of support in the UMBC Department of Computer
                 Science, I mentored 20 students each week, ensured they
                 understood fundamental coding concepts, held consistent office
                 hours, and even brainstormed new assignment ideas.
-            </p>
-        ),
+            `,
         skills: ['Python'],
     },
     {
         title: 'Computer Science Tutor @ UMBC',
         date: 'Mar. 2021 – Dec. 2021',
-        description: (
-            <p>
+        description: `
                 At the UMBC Academic Success Center, I led tutoring sessions for
                 two fundamental computer science courses, honing in on Python
                 basics and the OOP principles of C++, ensuring students grasped
                 key concepts with ease.
-            </p>
-        ),
+            `,
         skills: ['Python', 'C++'],
     },
     {
         title: 'DevOps Engineer Intern @ Northrop Grumman',
         date: 'May 2021 – Aug. 2021',
-        description: (
-            <p>
+        description: `
                 My internship at Northrop Grumman saw me pioneering the
                 automation of build and deployment processes for large-scale
                 ground system solutions in an agile environment.
-            </p>
-        ),
+            `,
         skills: ['Ansible'],
     },
 ];
--- src/data/projects.js
+++ src/data/projects.js
@@ -2,28 +2,24 @@ const projects = [
     {
         title: 'Coming soon...',
         subTitle: 'Python, FastAPI, React, Postgres, S3, Docker Compose',
-        description: (
-            <p>
+        description: `
                 Interactive photography portfolio designed to highlight my
                 photography over the years. Seamlessly manages and displays a
                 diverse collection of images, providing a visually engaging
                 experience.
-            </p>
-        ),
+            `,
         image: '/assets/images/project-1.webp',
         link: 'https://github.com/abel-asfaw/photography-portfolio',
     },
     {
         title: 'Personal Website',
         subTitle: 'JavaScript, React, Tailwind CSS, Vite.js',
-        description: (
-            <p>
+        description: `
                 Fully responsive web application showcasing my experience,
                 skills, and projects in order to provide an online presence for
                 potential employers. You're already looking at the demo, so
                 click to see source code.
-            </p>
-        ),
+            `,
         image: '/assets/svg/a-background.svg',
         link: 'https://github.com/abel-asfaw/personal-website',
     },
