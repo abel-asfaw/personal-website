7ae9094d1b274c700f82f512cd7255434471b09d
Abel <dev@abelasfaw.com>
2023-10-02 21:22:02 -0700

    style: remove icons from SectionHeader

--- src/components/common/Section/Section.jsx
+++ src/components/common/Section/Section.jsx
@@ -4,7 +4,7 @@ import { SectionHeader } from '.';
 export default function Section({ id, title, blurb, className, children }) {
     return (
         <section id={id} className="flex flex-col gap-6">
-            <SectionHeader id={id} title={title} blurb={blurb} />
+            <SectionHeader title={title} blurb={blurb} />
             <SectionContent className={className}>{children}</SectionContent>
         </section>
     );
--- src/components/common/Section/SectionHeader.jsx
+++ src/components/common/Section/SectionHeader.jsx
@@ -1,24 +1,9 @@
-import {
-    HiOutlineOfficeBuilding,
-    HiCode,
-    HiOutlineChip,
-    HiOutlineUserCircle,
-} from 'react-icons/hi';
-
-export default function SectionHeader({ id, title, blurb }) {
-    const icons = {
-        experience: HiOutlineOfficeBuilding,
-        projects: HiCode,
-        skills: HiOutlineChip,
-        contact: HiOutlineUserCircle,
-    };
-
-    const Icon = icons[id];
-
+export default function SectionHeader({ title, blurb }) {
     return (
         <header className="flex flex-col gap-2 text-center">
-            <Icon size={40} className="self-center text-neutral-400" />
-            <h1 className="font-cubano text-3xl text-neutral-100">{title}</h1>
+            <h1 className="font-cubano text-3xl text-neutral-100">
+                {title}
+            </h1>
             <p className="leading-relaxed">{blurb}</p>
         </header>
     );
