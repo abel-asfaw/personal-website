f6a69571bbf1be8d5eb32f31a26c468166f848d3
Abel <dev@abelasfaw.com>
2023-10-03 18:13:18 -0700

    refactor: make Underline component more modular by adding className prop for customization

--- src/components/common/Decorator/Underline.jsx
+++ src/components/common/Decorator/Underline.jsx
@@ -1,12 +1,19 @@
 import classNames from 'classnames';
 
-export default function Underline() {
+export default function Underline({ className }) {
     const transition = 'duration-300 ease-in';
     const baseClasses = '-mb-0.5 ml-auto w-0 rounded border-t border-zinc-950';
     const hoverClasses =
         'group-hover:ml-0 group-hover:w-full group-hover:border-zinc-200';
 
     return (
-        <div className={classNames(transition, baseClasses, hoverClasses)} />
+        <div
+            className={classNames(
+                transition,
+                baseClasses,
+                hoverClasses,
+                className
+            )}
+        />
     );
 }
