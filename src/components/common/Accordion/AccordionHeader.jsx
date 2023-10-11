54e218796e168118641890b11d33badac2343104
Abel <dev@abelasfaw.com>
2023-10-10 20:30:33 -0700

    style: disable text selection for Accordion header

--- src/components/common/Accordion/AccordionHeader.jsx
+++ src/components/common/Accordion/AccordionHeader.jsx
@@ -21,7 +21,7 @@ export default function AccordionHeader({
                 backgroundColor: isExpanded ? '#27272a' : '#18181b',
             }}
             transition={animationDuration}
-            className="flex cursor-pointer items-center gap-2 rounded px-2 py-4 sm:p-4"
+            className="flex cursor-pointer select-none items-center gap-2 rounded px-2 py-4 sm:p-4"
             onClick={onHeaderClick}
         >
             <AnimatePresence initial={false}>
