66e1c894b9b8c22fc8533bda80e901ffac657c4b
Abel <dev@abelasfaw.com>
2023-10-20 17:04:40 -0700

    style: revert "style: open the first accordion by default"
    This reverts commit 0db842c86d19c06e7a87e5b38edc04a917f500b1.

--- src/components/common/Accordion/Accordion.jsx
+++ src/components/common/Accordion/Accordion.jsx
@@ -3,7 +3,7 @@ import { AnimatePresence, motion } from 'framer-motion';
 import { useState } from 'react';
 
 export default function Accordion({ items }) {
-    const [expandedIndex, setExpandedIndex] = useState(0);
+    const [expandedIndex, setExpandedIndex] = useState(-1);
 
     const handleClick = (nextIndex) => {
         setExpandedIndex(nextIndex === expandedIndex ? -1 : nextIndex);
