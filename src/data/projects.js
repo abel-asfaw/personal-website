251327c048e204cd7e106d2dc083e5b37e3b669d
Abel <dev@abelasfaw.com>
2023-10-22 12:40:52 -0700

    edit: update project description

--- src/data/projects.js
+++ src/data/projects.js
@@ -6,8 +6,8 @@ const projects = [
         description: `
             I built a photography portolio web app where I can upload and
             display my photos in a simple but sleek grid format. I've set up
-            an admin page where I can manage the photos and it, along with
-            the endpoints, is secured through Auth0. Check it out!
+            an admin page where I can manage the photos and this page (along
+            with the API endpoints) is secured through Auth0. Check it out!
         `,
         image: '/assets/images/project-1.webp',
         link: 'https://github.com/abel-asfaw/photography-portfolio',
