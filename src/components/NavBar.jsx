48e123089b5932624dde0a5cdb4b933ea667739b
Abel <dev@abelasfaw.com>
2023-10-22 12:24:42 -0700

    refactor: simplify NavBar sizing logic

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -15,7 +15,7 @@ export default function Navbar() {
                 'full-bleed sticky top-0 z-10 bg-zinc-950/70 py-4 shadow-sm shadow-zinc-950 backdrop-blur'
             }
         >
-            <nav className="flex flex-wrap justify-center gap-4 font-russisch font-bold duration-700 sm:gap-24">
+            <nav className="flex flex-wrap justify-around font-russisch font-bold">
                 {navItems.map((item) => (
                     <LinkButton
                         key={item.label}
