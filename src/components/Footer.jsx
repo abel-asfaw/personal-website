832544b4943e173b9a72180ac0576c9232fa05b2
Abel <dev@abelasfaw.com>
2023-11-03 21:55:48 -0700

    style: add full-bleed to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -16,7 +16,7 @@ export default function Footer() {
     ];
 
     return (
-        <footer className="border-t border-zinc-900">
+        <footer className="full-bleed border-t border-zinc-900">
             <div className="flex flex-col items-center justify-center p-6 text-neutral-400 xs:flex-row xs:justify-between">
                 <p>© 2023 Abel Asfaw</p>
                 <div className="mt-2 flex flex-wrap gap-6 xs:mt-0">
