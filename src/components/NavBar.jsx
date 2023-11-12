065a2a1e6b861738adfe08c548baf39550234929
Abel <dev@abelasfaw.com>
2023-11-12 10:29:16 -0800

    style: make nav elements narrower

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -15,7 +15,7 @@ export default function Navbar() {
                 'full-bleed sticky top-0 z-10 bg-zinc-950/70 py-4 shadow-sm shadow-zinc-950 backdrop-blur'
             }
         >
-            <nav className="flex flex-wrap justify-center gap-6 font-russisch font-bold sm:gap-20">
+            <nav className="flex flex-wrap justify-center gap-6 font-russisch font-bold sm:gap-14">
                 {navItems.map((item) => (
                     <LinkButton
                         key={item.label}
