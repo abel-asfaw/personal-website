529cc104de0217076800875d83007ce9563c816e
Abel <dev@abelasfaw.com>
2024-01-14 09:45:34 -0800

    chore: update description for portfolio project

--- src/data/projects.js
+++ src/data/projects.js
@@ -8,12 +8,15 @@ const projects = [
             'Postgres',
             'AWS S3',
             'Docker',
+            'Tailwind CSS',
         ],
         description: `
             I built a photography portolio web app where I can upload and
-            display my photos in a simple but sleek grid format. I've set up
-            an admin page where I can manage the photos and this page (along
-            with the API endpoints) is secured through Auth0. Check it out!
+            display my photos without the clutter of social media, and so I can 
+            add my own personal touch to how my photos are displayed. I've set up
+            an admin page (which is secured through Auth0) where I can manage the 
+            photos. And I've implemented token-based access for the upload and 
+            delete API endpoints.
         `,
         image: '/assets/images/project-1.webp',
         link: 'https://github.com/abel-asfaw/photography-portfolio',
