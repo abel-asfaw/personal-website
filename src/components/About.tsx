fb4381cc26da04b4d58314130a62000ed96be124
Abel <dev@abelasfaw.com>
2025-09-14 01:40:01 -0400

    style: Adjust About Me style (button shadows/color, text width)

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -30,17 +30,17 @@ export function About({ id }: AboutProps) {
       <div className="relative h-auto w-64 after:pointer-events-none after:absolute after:inset-x-0 after:-bottom-px after:h-20 after:bg-gradient-to-t after:to-transparent after:content-[''] sm:w-72 sm:after:from-[#09080F]">
         <img src="/assets/images/me.png" alt="Portrait of Abel" />
       </div>
-      <h1 className="font-russisch -mb-1  text-3xl font-bold text-neutral-100 sm:text-4xl">
+      <h1 className="font-russisch -mb-1 text-3xl font-bold text-neutral-100 sm:text-4xl">
         {aboutMeFields?.intro}{' '}
         <span className="animate-wave bg-gradient-to-r from-teal-300 via-purple-500 to-orange-500 bg-clip-text text-transparent">
           {aboutMeFields?.tagLine}
         </span>
       </h1>
-      <p className="leading-relaxed">{aboutMeFields?.bio}</p>
+      <p className="w-full leading-relaxed sm:w-10/12">{aboutMeFields?.bio}</p>
       <div className="font-russisch flex gap-4 text-center font-bold transition-all">
         <LinkButton
           href="#projects"
-          className="bg-green-600 text-white hover:bg-green-700"
+          className="drop-shadow-md/25 bg-green-600 text-white hover:bg-green-700"
           promptButton
         >
           See my work
@@ -48,7 +48,7 @@ export function About({ id }: AboutProps) {
         </LinkButton>
         <LinkButton
           href="mailto:contact@abelasfaw.com"
-          className="border-[1px] border-zinc-700 bg-transparent text-neutral-400 hover:bg-zinc-700 hover:text-white"
+          className="drop-shadow-md/25 border-[.5px] border-zinc-700 bg-zinc-900 hover:bg-zinc-700 hover:text-white"
           promptButton
           target="_blank"
           rel="noopener noreferrer"
