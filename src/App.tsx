5a348744c8b5771761afbcd4e5e7d2766cf5c71b
Abel <dev@abelasfaw.com>
2026-02-12 21:05:25 -0500

    refactor: Replace MainContent component with inline content in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
+import { About } from './components/About';
+import { Experience } from './components/Experience';
 import { Footer } from './components/Footer';
-import { MainContent } from './components/MainContent';
 import { Navbar } from './components/NavBar';
+import { Projects } from './components/Projects';
+import { Skills } from './components/Skills';
 
 export default function App() {
   return (
     <div className="font-dmsans flex flex-col gap-12 text-neutral-200 sm:gap-16">
       <Navbar />
-      <MainContent />
+      <main className="wrapper flex flex-col gap-20 sm:gap-32">
+        <About id="about" />
+        <Experience />
+        <Projects />
+        <Skills />
+      </main>
       <Footer />
     </div>
   );
