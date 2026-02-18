adc0ab14864a8384a5faca865be79e12f5e06898
Abel <dev@abelasfaw.com>
2026-02-18 11:35:26 -0500

    feat: Refactor useContentfulData to add loading state

--- src/App.tsx
+++ src/App.tsx
@@ -7,17 +7,25 @@ import { Skills } from './components/Skills';
 import { useContentfulData } from './contentful';
 
 export default function App() {
-  const { aboutMe, experienceSection, projectsSection, skillsSection } =
-    useContentfulData();
+  const {
+    data: { aboutMe, experienceSection, projectsSection, skillsSection },
+    isPending,
+  } = useContentfulData();
 
   return (
-    <div className="font-dmsans flex flex-col gap-12 text-neutral-200 sm:gap-16">
+    <div className="font-dmsans grid min-h-screen grid-rows-[auto_1fr_auto] gap-12 text-neutral-200 sm:gap-16">
       <Navbar />
-      <main className="wrapper flex flex-col gap-20 sm:gap-32">
-        <About aboutMe={aboutMe} />
-        <Experience experienceSection={experienceSection} />
-        <Projects projectsSection={projectsSection} />
-        <Skills skillsSection={skillsSection} />
+      <main className="wrapper gap-20 sm:gap-32">
+        {isPending ? (
+          <div className="place-self-center">Loading...</div>
+        ) : (
+          <>
+            <About aboutMe={aboutMe} />
+            <Experience experienceSection={experienceSection} />
+            <Projects projectsSection={projectsSection} />
+            <Skills skillsSection={skillsSection} />
+          </>
+        )}
       </main>
       <Footer />
     </div>
--- src/contentful/useContentfulData.ts
+++ src/contentful/useContentfulData.ts
@@ -9,46 +9,51 @@ import type {
 } from './contentful.types';
 
 export function useContentfulData() {
+  const queriesResult = useQueries({
+    queries: [
+      {
+        queryKey: ['aboutMe'],
+        queryFn: () =>
+          client.getEntries<TypeAboutMeSkeleton>({
+            content_type: 'aboutMe',
+          }),
+      },
+      {
+        queryKey: ['experienceSection'],
+        queryFn: () =>
+          client.getEntries<TypeExperienceSectionSkeleton>({
+            content_type: 'experienceSection',
+          }),
+      },
+      {
+        queryKey: ['projectsSection'],
+        queryFn: () =>
+          client.getEntries<TypeProjectsSectionSkeleton>({
+            content_type: 'projectsSection',
+          }),
+      },
+      {
+        queryKey: ['skillsSection'],
+        queryFn: () =>
+          client.getEntries<TypeSkillsSkeleton>({
+            content_type: 'skillsSection',
+          }),
+      },
+    ],
+  });
+
   const [aboutMe, experienceSection, projectsSection, skillsSection] =
-    useQueries({
-      queries: [
-        {
-          queryKey: ['about-me'],
-          queryFn: () =>
-            client.getEntries<TypeAboutMeSkeleton>({
-              content_type: 'aboutMe',
-            }),
-        },
-        {
-          queryKey: ['experienceSection'],
-          queryFn: () =>
-            client.getEntries<TypeExperienceSectionSkeleton>({
-              content_type: 'experienceSection',
-            }),
-        },
-        {
-          queryKey: ['projectsSection'],
-          queryFn: () =>
-            client.getEntries<TypeProjectsSectionSkeleton>({
-              content_type: 'projectsSection',
-            }),
-        },
-        {
-          queryKey: ['skillsSection'],
-          queryFn: () =>
-            client.getEntries<TypeSkillsSkeleton>({
-              content_type: 'skillsSection',
-            }),
-        },
-      ],
-    });
+    queriesResult;
 
   return {
-    aboutMe: aboutMe.data?.items[0]?.fields,
-    experienceSection: experienceSection.data?.items[0]?.fields,
-    projectsSection: projectsSection.data?.items[0]?.fields,
-    skillsSection: skillsSection.data?.items[0]?.fields,
+    data: {
+      aboutMe: aboutMe.data?.items[0]?.fields,
+      experienceSection: experienceSection.data?.items[0]?.fields,
+      projectsSection: projectsSection.data?.items[0]?.fields,
+      skillsSection: skillsSection.data?.items[0]?.fields,
+    },
+    isPending: queriesResult.some(query => query.isPending),
   };
 }
 
-export type ContentfulData = ReturnType<typeof useContentfulData>;
+export type ContentfulData = ReturnType<typeof useContentfulData>['data'];
